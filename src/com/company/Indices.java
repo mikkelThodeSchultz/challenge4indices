c1709f3f8d0043a7849c75effb5c9768e46c69ac
mikkelThodeSchultz <mikk75c4@stud.kea.dk>
2022-04-28 13:32:28 +0200

    added notes

--- src/com/company/Indices.java
+++ src/com/company/Indices.java
@@ -6,15 +6,19 @@ public class Indices {
     public ArrayList<Integer> indices(String text, String pattern) {
         ArrayList<Integer> returnArray = new ArrayList<>();
         String replacementString = "";
+        // replacement string for the patterne so i can find the next instance of the pattern
         for (int i = 0; i < pattern.length(); i++) {
             replacementString = replacementString + "a";
         }
+        // in case the patterne is only a's this makes sure that the replacement string is different
         if (replacementString.equals(pattern)) {
             replacementString = "";
             for (int i = 0; i < pattern.length(); i++) {
                 replacementString = replacementString + "b";
             }
         }
+        // finds the index og the patterne, adds in to the array and replaces it with the replacement string,
+        // so it can find the next one
         while (text.contains(pattern)) {
             int indexOfPattern = text.indexOf(pattern);
             returnArray.add(indexOfPattern);
